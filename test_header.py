7d9ee4d89ef0ddb1c56e87b3100bb6e99ec071cd
fuenukkusu2501 <fenixxx2501@gmail.com>
2024-01-15 20:49:54 +0300

    s

--- test_header.py
+++ test_header.py
@@ -3,6 +3,8 @@ from playwright.sync_api import expect
 def test_header_link(page):
     page.goto("https://garwin.ru/", wait_until='domcontentloaded')
     expect(page).to_have_url("https://garwin.ru/")
+    response = page.request.get('https://garwin.ru/')
+    expect(response).not_to_be_ok
     page.get_by_role("banner").get_by_role("link", name="Акции").click(force=True)
     page.get_by_role("banner").get_by_role("link", name="Новости").click(force=True)
     page.get_by_role("banner").get_by_role("link", name="Круг друзей").click()
