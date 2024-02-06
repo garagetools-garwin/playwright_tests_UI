d4120007ad80e88dd8506b8d64a43bd6fe21dd6b
fuenukkusu2501 <52046128+fuenukkusu2501@users.noreply.github.com>
2024-02-06 11:07:10 +0300

    Delete test_drafts directory

--- test_drafts/test_login.py
+++
@@ -1,9 +0,0 @@
-
-def test_header_link(page):
-    page.goto('https://automationexercise.com/login', wait_until="domcontentloaded")
-    with page.expect_response("https://automationexercise.com/login") as response_info:
-        page.locator('[data-qa="login-email"]').fill("asd@f.rf")
-        page.locator('[data-qa="login-password"]').fill("a")
-        page.locator('[data-qa="login-button"]').click()
-        response = response_info.value
-        assert response.status == 200
