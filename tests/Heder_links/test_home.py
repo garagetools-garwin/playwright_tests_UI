822aa2e1502ed93ef655090e6424a8e87481eb43
fuenukkusu2501 <fenixxx2501@gmail.com>
2024-09-20 18:36:15 +0300

    Проставил айдишник к тесту и адаптер мод=0

--- tests/Heder_links/test_home.py
+++ tests/Heder_links/test_home.py
@@ -1,8 +1,12 @@
 import re
+import testit
+import allure
 import pytest
 from playwright.sync_api import expect
 
-
+@allure.title("Переход на домашнюю страницу")
+@testit.workItemIds("184") # ручной
+@testit.externalID("137583ce6919fca3ae3d379bc6a5007849e6853ff907b7610f761fa8131db009") # авто 169
 @pytest.mark.test_for_ci
 def test_home(page_fixture):
     url = "https://garwin.ru/"
