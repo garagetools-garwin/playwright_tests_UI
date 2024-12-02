8a4a21c689c40e2e6c8ba9d1df84c8da5b129ec2
fuenukkusu2501 <fenixxx2501@gmail.com>
2024-12-02 17:58:14 +0300

    Добавил тесты на промокод

--- page_objects/checkout_page.py
+++ page_objects/checkout_page.py
@@ -17,14 +17,16 @@ class CheckoutPage:
     PATH = "/checkout"
 
     """Промокод"""
-    PROMO_CODE_FIELD_INFO = ".Field.CartPromo__Field .Field__Info .Field__Text"
+    PROMO_CODE_FIELD_INFO = ".Field__Info .Field__Text"
     CLEAR_PROMO_CODE_FIELD_BUTTON = ".flexRow-C.FieldControls__Button.active"
     PROMO_CODE_FIELD = ".kit-input.Field__Input.disable-label"
-    PROMO_CODE_HINT_ICON = ".flexRow-AIC.Tooltip.CartPromo__Tooltip"
+    PROMO_CODE_FIELD_CHECK = ".Field.PromoWidget__Field.is-small.has-controls"
+    PROMO_CODE_HINT_ICON = "div.Tooltip__Icon"
     PROMO_CODE_HINT_POPUP = ".Tooltip__Inner.v-enter-to .Tooltip__Content"
     CANCEL_PROMO_CODE_BUTTON = ".flexRow-AIC.PromoWidget__CancelButton"
     PROMO_CODE_TOGGLE_BUTTON = ".PromoWidget__ToggleButton"
     PROMO_CODE_APPLY_BUTTON = ".PromoWidget__SubmitButton.Button.size--normal.color--secondary"
+    PRODUCT_PRICE = "p.CheckoutDeliveryProduct__Price"
 
     """Чек-аут"""
     ORDER_BUTTON = ".OrderTotal__Button.Button.size--medium.color--primary"
@@ -46,11 +48,13 @@ class CheckoutPage:
 
     @allure.step("Отменяю промокод если он был установлен")
     def check_promo_code(self):
+        self.open_promo_code_bar()
         if self.page.locator(self.CANCEL_PROMO_CODE_BUTTON).is_visible():
             self.page.locator(self.CANCEL_PROMO_CODE_BUTTON).click()
         else:
-            self.fill_valid_promo_code()
-            self.click_apply_button()
+            return
+            # self.fill_valid_promo_code()
+            # self.click_apply_button()
 
     @allure.step("Открываю блок 'Промокод'")
     def open_promo_code_bar(self):
@@ -61,7 +65,7 @@ class CheckoutPage:
         self.open_promo_code_bar()
 
     def promo_code_bar(self):
-        return self.page.locator(".CartPromo__ToggleButton")
+        return self.page.locator(self.PROMO_CODE_TOGGLE_BUTTON)
 
     @allure.step("Ввожу валидный промокод")
     def fill_valid_promo_code(self):
@@ -74,6 +78,26 @@ class CheckoutPage:
     @allure.step("Нажимаю 'Применить'")
     def click_apply_button(self):
         self.page.locator(self.PROMO_CODE_APPLY_BUTTON).click()
+
+    # Версия активации когда не нужно запоминать цену
+    # @allure.step("Активирую валидный промокод")
+    # def activate_valid_promo_code(self):
+    #     self.open_promo_code_bar()
+    #     self.check_promo_code()
+    #     self.fill_valid_promo_code()
+    #     self.click_apply_button()
+
+
+    @allure.step("Активирую валидный промокод")
+    def activate_valid_promo_code(self):
+        self.fill_valid_promo_code()
+        self.click_apply_button()
+
+    @allure.step("Активирую невалидный промокод")
+    def activate_invalid_promo_code(self):
+        # self.open_promo_code_bar()
+        self.fill_invalid_promo_code()
+        self.click_apply_button()
 
     # Подсказка поля промокод
     def promo_code_field_info(self):
@@ -89,7 +113,7 @@ class CheckoutPage:
 
     # Поле промокод
     def promo_code_field(self):
-        return self.page.locator(self.PROMO_CODE_FIELD)
+        return self.page.locator(self.PROMO_CODE_FIELD_CHECK)
 
     @allure.step("Навожу курсор на подсказку")
     def hover_to_promo_code_hint(self):
@@ -103,3 +127,19 @@ class CheckoutPage:
     def promo_code_hint_popup_text(self):
         return self.page.locator(self.PROMO_CODE_HINT_POPUP).inner_text()
 
+
+    """Блок Калькуляции"""
+
+
+    # @allure.step("Запоминаю стоимость скидки")
+    # def discounted_price(self):
+    #     text_discounted_price = self.page.locator(self.PRODUCT_PRICE_DISCOUNTED).inner_text()
+    #     discounted_price_number = float(text_discounted_price.replace('\n\n\xa0\n\n₽', '').replace(',', '.'))
+    #     return discounted_price_number
+
+
+    @allure.step("Запоминаю первоначальную стоимость товара")
+    def base_price(self):
+        text_base_price = self.page.locator(self.PRODUCT_PRICE).inner_text()
+        base_price_number = float(text_base_price.replace('\n\n\xa0\n\n₽', '').replace(',', '.').replace(' ₽/шт.', ''))
+        return base_price_number
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -3,122 +3,230 @@ import pytest
 import allure
 
 from playwright.sync_api import expect
+from page_objects.checkout_page import CheckoutPage
 from page_objects.cart_page import CartPage
+
+
 from page_objects.autorization_modal_element import AutorizationModalElement
 from page_objects.header_element import HeaderElement
 
 
-
 """Промокод"""
 
+
 #TODO добавить к тестам test_
+#TODO перевести методы связаные с блоком калькуляции в checkout_page
+
+@pytest.mark.auth
 @allure.title("Применение валидного промокода")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_aplying_a_valid_promo_code(page_fixture, base_url):
+def test_checkout_aplying_a_valid_promo_code(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
-    cart_page.add_to_cart_not_discounted_product(base_url)
     cart_page.open(base_url)
-    cart_page.activate_valid_promo_code()
-    with allure.step("Проверяю, что скидка на плашке равна 5%"):
-        assert cart_page.text_discount_budget() == "-5%"
+    cart_page.clear_cart()
+    cart_page.add_to_cart_not_discounted_product(base_url)
+    checkout_page.open(base_url)
+    checkout_page.check_promo_code()
+    with allure.step("Запоминю цену товара до активации промокода"):
+        base_price = checkout_page.base_price()
+        checkout_page.activate_valid_promo_code()
+    with allure.step("Запоминаю цену товара после активации промокода"):
+        discounted_price = checkout_page.base_price()
     with allure.step("Проверяю, что цена товара равна его первоначальной стоимости - 5%"):
-        assert cart_page.discounted_price() == round(cart_page.base_price() * 0.95, 1)
+        assert discounted_price == round(base_price * 0.95, 2)
 
 
+@pytest.mark.auth
 @allure.title("Применение невалидного промокода")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_aplying_a_invalid_promo_code(page_fixture, base_url):
+def test_checkout_aplying_a_invalid_promo_code(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
-    cart_page.add_to_cart_not_discounted_product(base_url)
     cart_page.open(base_url)
-    with allure.step("Запоминую цену товара до активации промокода"):
-        pre_action_price = cart_page.order_total_price()
-        cart_page.activate_invalid_promo_code()
-    with allure.step("Запоминую цену товара после активации промокода"):
-        post_action_price = cart_page.order_total_price()
+    cart_page.clear_cart()
+    cart_page.add_to_cart_not_discounted_product(base_url)
+    checkout_page.open(base_url)
+    checkout_page.check_promo_code()
+    with allure.step("Запоминю цену товара до активации промокода"):
+        pre_action_price = checkout_page.base_price()
+        checkout_page.activate_invalid_promo_code()
+    with allure.step("Запоминаю цену товара после активации промокода"):
+        post_action_price = checkout_page.base_price()
     with allure.step("Проверяю, что цена товара не изменилась"):
         assert pre_action_price == post_action_price
-    with allure.step("Проверяю, что плашка со скидкой не отображается"):
-        expect(cart_page.discount_budget()).not_to_be_visible()
     with allure.step("Проверяю, что на странице отображается подсказка 'Промокод не действителен'"):
-        expect(cart_page.promo_code_field_info()).to_have_text("Промокод не действителен")
+        expect(checkout_page.promo_code_field_info()).to_have_text("Промокод не действителен")
 
 
+@pytest.mark.auth
 @allure.title("Раскрытие блока 'Промокод'")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_promo_code_block(page_fixture, base_url):
+def test_checkout_promo_code_block(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
-    cart_page.add_to_cart_not_discounted_product(base_url)
     cart_page.open(base_url)
-    cart_page.open_promo_code_bar()
+    cart_page.clear_cart()
+    cart_page.add_to_cart_not_discounted_product(base_url)
+    checkout_page.open(base_url)
+    checkout_page.open_promo_code_bar()
     with allure.step("Запоминаю положение блока 'Промокод'"):
-        promo_code_bar_status = cart_page.promo_code_bar()
+        promo_code_bar_status = checkout_page.promo_code_bar()
     with allure.step("Проверяю, что блок 'Промокод' - раскрыт "):
-        expect(promo_code_bar_status).to_have_class('CartPromo__ToggleButton active')
-    cart_page.close_promo_code_bar()
+        expect(promo_code_bar_status).to_have_class('PromoWidget__ToggleButton --is-active')
+    checkout_page.close_promo_code_bar()
     with allure.step("Проверяю, что блок 'Промокод' - закрыт "):
-        expect(promo_code_bar_status).not_to_have_class('CartPromo__ToggleButton active')
+        expect(promo_code_bar_status).not_to_have_class('PromoWidget__ToggleButton --is-active')
 
 
+@pytest.mark.auth
 @allure.title("Применение валидного промокода к неподходящему товару")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_aplying_a_valid_promo_code_on_not_stm_product(page_fixture, base_url):
+def test_checkout_aplying_a_valid_promo_code_on_not_stm_product(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
+    cart_page.open(base_url)
+    cart_page.clear_cart()
     cart_page.add_to_cart_not_stm_product(base_url)
-    cart_page.open(base_url)
-    cart_page.activate_valid_promo_code()
-    with allure.step("Проверяю, что плашка со скидкой не отображается"):
-        expect(cart_page.discount_budget()).not_to_be_visible()
+    checkout_page.open(base_url)
+    checkout_page.check_promo_code()
+    checkout_page.activate_valid_promo_code()
     with allure.step("Проверяю, что на странице отображается подсказка 'В корзине нет товаров, к которым можно применить введенный промокод'"):
-        expect(cart_page.promo_code_field_info()).to_have_text("В корзине нет товаров, к которым можно применить введенный промокод")
+        expect(checkout_page.promo_code_field_info()).to_have_text("В корзине нет товаров, к которым можно применить введенный промокод")
 
 
+@pytest.mark.auth
 @allure.title("Отмена промокода")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_canceling_promo_code(page_fixture, base_url):
+def test_checkout_canceling_promo_code(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
-    cart_page.add_to_cart_not_discounted_product(base_url)
     cart_page.open(base_url)
-    with allure.step("Запоминую цену товара до активации промокода"):
-        pre_action_price = cart_page.order_total_price()
-    cart_page.activate_valid_promo_code()
-    cart_page.cancel_promo_code()
+    cart_page.clear_cart()
+    cart_page.add_to_cart_not_discounted_product(base_url)
+    checkout_page.open(base_url)
+    checkout_page.check_promo_code()
+    with allure.step("Запоминю цену товара до активации промокода"):
+        pre_action_price = checkout_page.base_price()
+    checkout_page.activate_valid_promo_code()
+    checkout_page.cancel_promo_code()
     with allure.step("Запоминую цену товара после отмены промокода"):
-        post_action_price = cart_page.order_total_price()
+        post_action_price = checkout_page.base_price()
     with allure.step("Проверяю, что цена до ативации промокода и после его отмены -равны"):
         assert pre_action_price == post_action_price
-    with allure.step("Проверяю, что плашка со скидкой не отображается"):
-        expect(cart_page.discount_budget()).not_to_be_visible()
     with allure.step("Проверяю, что поле 'Промокод' - пустое"):
-        expect(cart_page.promo_code_field()).to_have_attribute("data-empty", "true")
+        expect(checkout_page.promo_code_field()).to_have_attribute("data-empty", "true")
 
 
+@pytest.mark.auth
 @allure.title("Очистка поля 'Промокод'")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_cleaning_intput_promo_code(page_fixture, base_url):
+def test_checkout_cleaning_intput_promo_code(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
-    cart_page.add_to_cart_not_discounted_product(base_url)
     cart_page.open(base_url)
-    cart_page.open_promo_code_bar()
-    cart_page.fill_valid_promo_code()
-    cart_page.clear_promo_code_field_by_cross()
+    cart_page.clear_cart()
+    cart_page.add_to_cart_not_discounted_product(base_url)
+    checkout_page.open(base_url)
+    checkout_page.check_promo_code()
+    checkout_page.fill_valid_promo_code()
+    checkout_page.clear_promo_code_field_by_cross()
     with allure.step("Проверяю, что поле 'Промокод' - пустое"):
-        expect(cart_page.promo_code_field()).to_have_attribute("data-empty", "true")
+        expect(checkout_page.promo_code_field()).to_have_attribute("data-empty", "true")
 
 
+@pytest.mark.auth
 @allure.title("Активация подсказки")
-@pytest.mark.skip("Архив. блок 'Промокод' удалили из корзины")
-def checkout_activating_a_hint(page_fixture, base_url):
+def test_checkout_activating_a_hint(page_fixture, base_url):
+    checkout_page = CheckoutPage(page_fixture)
     cart_page = CartPage(page_fixture)
-    cart_page.add_to_cart_not_discounted_product(base_url)
     cart_page.open(base_url)
-    cart_page.open_promo_code_bar()
-    cart_page.hover_to_promo_code_hint()
+    cart_page.clear_cart()
+    cart_page.add_to_cart_not_discounted_product(base_url)
+    checkout_page.open(base_url)
+    checkout_page.check_promo_code()
+    checkout_page.hover_to_promo_code_hint()
     with allure.step("Проверяю, что подсказка отображается на странице"):
-        expect(cart_page.promo_code_hint_popup()).to_be_visible()
+        expect(checkout_page.promo_code_hint_popup()).to_be_visible()
     with allure.step("Проверяю, что текст подсказки соответствует шаблону"):
-        assert cart_page.promo_code_hint_popup_text() == "Промокод действует на отдельные товары и в ограниченный период времени. Подробные условия действия промокода вы можете узнать в описании акции, при получении промокода или у наших специалистов."
+        assert checkout_page.promo_code_hint_popup_text() == "Промокод действует на отдельные товары и в ограниченный период времени. Подробные условия действия промокода вы можете узнать в описании акции, при получении промокода или у наших специалистов."
 
 # Методы манипулиции с промкодом
-# checkout_page.open(base_url)
 # checkout_page.price_changes_with_a_promo_code()
+
+"""Тесты для блока калькуляции"""
+
+
+@allure.title("Изменение цены в блоке калькуляции по всем позициям")
+def calculation_block_calculate_price_for_all_products(page_fixture, base_url):
+    cart_page = CartPage(page_fixture)
+    cart_page.add_to_cart_multiple_products(base_url)
+    cart_page.open(base_url)
+    total_price = cart_page.calculate_total_price()
+    cart_page.get_cart_prices()
+    cart_page.compare_prices(total_price)
+
+
+@allure.title("Изменение цены в блоке калькуляции по части позиций (Чек-бокс)")
+def calculation_block_calculate_price_of_some_products(page_fixture, base_url):
+    cart_page = CartPage(page_fixture)
+    cart_page.add_to_cart_multiple_products(base_url)
+    cart_page.open(base_url)
+    cart_page.click_to_checkbox_for_all_products()
+    cart_page.order_price_is_zero()
+    cart_page.click_first_checkbox_product()
+    cart_page.calculate_total_price_for_checked_products()
+    cart_page.click_second_checkbox_product()
+    cart_page.calculate_total_price_for_checked_products()
+
+
+@allure.title("Отправка на печать")
+def cart_print_form_activation(page_fixture, base_url):
+    cart_page = CartPage(page_fixture)
+    cart_page.add_to_cart(base_url)
+    cart_page.open(base_url)
+    cart_page.click_print_button()
+    with allure.step("Проверяю, что окно печати на странице"):
+        page_fixture.wait_for_function("window.waitForPrintDialog")
+
+
+@allure.title("Изменение количества товара через счетчик")
+def cart_counter(page_fixture, base_url):
+    cart_page = CartPage(page_fixture)
+    cart_page.add_to_cart(base_url)
+    cart_page.open(base_url)
+    with allure.step("Проверяю, что количество товара меняется"):
+        with allure.step("Проверяю, что количество выбранной позиции = 1"):
+            quantity = cart_page.get_quantity_of_product()
+            assert quantity[0] == 1
+        with allure.step("Увеличиваю количество товара на 1"):
+            page_fixture.locator(".Counter__Element").nth(1).click()
+        with allure.step("Проверяю, что количество выбранной позиции = 2"):
+            quantity = cart_page.get_quantity_of_product()
+            assert quantity[0] == 2
+        with allure.step("Уменьшаю количество товара на 1"):
+            page_fixture.locator(".Counter__Element").nth(0).click()
+        with allure.step("Проверяю, что количество выбранной позиции = 1"):
+            quantity = cart_page.get_quantity_of_product()
+            assert quantity[0] == 1
+
+
+@allure.title("Изменение цены в блоке калькуляции по части позиций (Счетчик)")
+def cart_calculation_block_calculate_price_of_some_products(page_fixture, base_url):
+    cart_page = CartPage(page_fixture)
+    cart_page.add_to_cart(base_url)
+    cart_page.open(base_url)
+    with allure.step("Запоминаю цену товара"):
+        price_text = page_fixture.locator('.AvailableList.CartPage__AvailableProductList .Price__Value').nth(0).inner_text()
+        price = int(price_text.replace('\xa0', '').replace(' ', ''))
+    with allure.step("Запоминаю общую сумму заказа"):
+        total_price = cart_page.order_total_price()
+    with allure.step("Проверяю, что цена товара соответствует сумме заказа"):
+        assert price == total_price
+    with allure.step("Увеличиваю количество товара на 1"):
+        page_fixture.locator(".Counter__Element").nth(1).click()  #первый плюс
+    with allure.step("Считаю стоимость товара в колличестве 2-х единиц"):
+        new_price = price+price
+    with allure.step("Запоминаю суму заказа"):
+        total_price = cart_page.order_total_price()
+    with allure.step("Проверяю, что стоимость двух товаров равняется сумме заказа"):
+        assert new_price == total_price
+    with allure.step("Уменьшаю количество товара на 1"):
+        page_fixture.locator(".Counter__Element").nth(0).click()  # первый плюс
+    with allure.step("Проверяю, что сумма заказа равна сумме одной единице товара"):
+        total_price = cart_page.order_total_price()
+        assert price == total_price
